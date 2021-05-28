19e5556dc34c479de3002536ec124360780185d2
95yuki0305 <matsuyuki0305@gmail.com>
2021-05-28 12:26:40 +0900

    ユーザー管理機能・仮完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  with_options presence: true do
-    validates :nickname, presence: true
-    validates :password, :password_confirmation, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, allow_blank: true}
-  end
+  validates :password, :password_confirmation, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, allow_blank: true}
 end
