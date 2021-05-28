2b6ef160eeeaec6ee74984d5a1d888643c851444
95yuki0305 <matsuyuki0305@gmail.com>
2021-05-28 13:12:38 +0900

    各モデル作成・アソシエーションも記述
    questionモデル,answerモデル作成
    マイグレーションファイル作成・実行
    各モデル(user.question.answer)のアソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many :questions
+  has_many :answers
+  
   validates :nickname, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   validates :password, :password_confirmation, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, allow_blank: true }
 end
