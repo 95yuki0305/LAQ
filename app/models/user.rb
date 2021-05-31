736554805103143dce1c6bb784a36498e4754f2d
95yuki0305 <matsuyuki0305@gmail.com>
2021-05-31 14:52:26 +0900

    Rubocop使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-
   has_many :questions
-  
+
   validates :nickname, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
