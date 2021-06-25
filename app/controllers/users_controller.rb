99b21de48c9ad9a6f0e49a4f02c9dc83f882d302
95yuki0305 <matsuyuki0305@gmail.com>
2021-06-25 15:17:23 +0900

    未ログインユーザーは解答のurlを直接入力しても遷移できない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+
   def show
     @nickname = current_user.nickname
     @question = current_user.questions
     @date = Date.current.strftime('%Y/%m/%d')
   end
+
 end
